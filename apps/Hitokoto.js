31678c901a9a676dada13cefb21b354ec3b7f330
洛洛 <2443958507@qq.com>
2025-02-10 18:09:37 +0800

    优化细节

--- apps/Hitokoto.js
+++ apps/Hitokoto.js
@@ -57,19 +57,9 @@ export class Hitokoto extends plugin {
       let json = await jsonresponse.json();
 
       if (json.hitokoto) {
-        let templateMessage = `ID: ${json.id}
-UUID: ${json.uuid}
-类型: ${json.type}
-句子: ${json.hitokoto}
-作者: ${json.from}
-来自: ${json.from_who || "未知"}
-创建者: ${json.creator}
-创建者UID: ${json.creator_uid}
-审核者: ${json.reviewer}
-提交来源: ${json.commit_from}
-创建时间: ${new Date(parseInt(json.created_at) * 1000).toLocaleString()}
-长度: ${json.length}`;
-        e.reply(templateMessage);
+        e.reply(
+          `ID: ${json.id}\nUUID: ${json.uuid}\n类型: ${json.type}\n句子: ${json.hitokoto}\n作者: ${json.from}\n来自: ${json.from_who || "未知"}\n创建者: ${json.creator}\n创建者UID: ${json.creator_uid}审核者: ${json.reviewer}提交来源: ${json.commit_from}\n创建时间: ${new Date(parseInt(json.created_at) * 1000).toLocaleString()}\n长度: ${json.length}`,
+        );
       } else if (json.status && json.message) {
         e.reply(`错误码: ${json.status}\n错误信息: ${json.message}`);
       } else {
