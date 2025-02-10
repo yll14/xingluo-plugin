7a62cca6918661ecfffd901c47a7b5ba2cca87c3
洛洛 <2443958507@qq.com>
2025-02-10 14:59:48 +0800

    优化细节

--- guoba.support.js
+++ guoba.support.js
@@ -25,35 +25,8 @@ export function supportGuoba() {
     configInfo: {
       schemas: [
         {
-          field: "updatetask.switch",
-          label: "功能开关",
-          bottomHelpMessage: "是否开启",
-          component: "Switch",
-        },
-        {
-          field: "updatetask.customRepository",
-          label: "自定义仓库地址",
-          helpMessage: "支持添加多个仓库地址，使用英文逗号分隔",
-          bottomHelpMessage:
-            "格式：https://gitee.com/用户名/仓库名,https://gitee.com/用户名/仓库名",
-          component: "Input",
-          required: true,
-          componentProps: {
-            placeholder: "请输入Gitee仓库地址，多个地址用英文逗号分隔",
-          },
-        },
-        {
-          field: "updatetask.cron",
-          label: "定时任务表达式",
-          helpMessage: "设置自动更新检查的时间",
-          bottomHelpMessage:
-            "默认：0 0 * * * ?（每天0点执行） 不知道怎么写前往<https://www.jyshare.com/front-end/9444/>获取帮助",
-          component: "Input",
-          required: true,
-          componentProps: {
-            placeholder:
-              "请输入cron表达式 不知道怎么写前往<https://www.jyshare.com/front-end/9444/>获取帮助",
-          },
+          label: '一言配置',
+          component: 'SOFT_GROUP_BEGIN'
         },
         {
           field: "Hitokoto.switch",
@@ -102,6 +75,41 @@ export function supportGuoba() {
           componentProps: {
             placeholder:
               "请输入一言类型，选择多种类型使用英文逗号连接例如`a,b,c`",
+          },
+        },
+        {
+          label: '自动检查更新配置',
+          component: 'SOFT_GROUP_BEGIN'
+        },
+        {
+          field: "updatetask.switch",
+          label: "功能开关",
+          bottomHelpMessage: "是否开启",
+          component: "Switch",
+        },
+        {
+          field: "updatetask.customRepository",
+          label: "自定义仓库地址",
+          helpMessage: "支持添加多个仓库地址，使用英文逗号分隔",
+          bottomHelpMessage:
+            "格式：https://gitee.com/用户名/仓库名,https://gitee.com/用户名/仓库名",
+          component: "Input",
+          required: true,
+          componentProps: {
+            placeholder: "请输入Gitee仓库地址，多个地址用英文逗号分隔",
+          },
+        },
+        {
+          field: "updatetask.cron",
+          label: "定时任务表达式",
+          helpMessage: "设置自动更新检查的时间",
+          bottomHelpMessage:
+            "默认：0 0 * * * ?（每天0点执行） 不知道怎么写前往<https://www.jyshare.com/front-end/9444/>获取帮助",
+          component: "Input",
+          required: true,
+          componentProps: {
+            placeholder:
+              "请输入cron表达式 不知道怎么写前往<https://www.jyshare.com/front-end/9444/>获取帮助",
           },
         },
       ],
