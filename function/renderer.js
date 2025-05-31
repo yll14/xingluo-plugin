379357c29bac018f167c2ca87d5518cf3c81dd90
洛洛 <2443958507@qq.com>
2025-05-31 02:13:55 +0000

    优化细节
    Signed-off-by: 洛洛 <2443958507@qq.com>

--- function/renderer.js
+++ function/renderer.js
@@ -30,7 +30,7 @@ export default async function (path, params, cfg) {
     sys: {
       scale: `style=transform:scale(${cfg.scale || 1})`,
       copyright: `Yunzai-Bot<span class="version">${Version.yunzai}</span> & ${PluginName_zh}<span class="version">${lobal.XINGLUO.PluginVersion}</span>&
-        ${PluginAuthor}`,
+        ${lobal.XINGLUO.PluginAuthor}`,
     },
     quality: 100,
   };
